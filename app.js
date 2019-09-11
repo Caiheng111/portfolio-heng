6bc4267a91f71e593421a03323dda6cfc4672545
Cai heng <ianyaheng822@gmail.com>
2019-09-11 20:49:59 +1000

    add the js animation for meun button

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const hambuger=document.querySelector(".menu_hambuger");
 const navLinks=document.querySelector(".nav_links");
 const links=document.querySelectorAll(".nav_links li");
+const menu=document.querySelector(".menu");
+const navInfo=document.querySelector(".info");
 
 hambuger.addEventListener("click",()=>{
   navLinks.classList.toggle("open");
@@ -10,5 +12,12 @@ hambuger.addEventListener("click",()=>{
     link.classList.toggle("fade");
   });
 });
+
+
+menu.addEventListener("click",()=>{
+  navLinks.classList.toggle("open");
+  navInfo.className="hidden";
+
+})
 
 
