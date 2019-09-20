7a69a27defc845aefc0016311d2283cedf1a031d
Cai heng <ianyaheng822@gmail.com>
2019-09-20 14:28:59 +1000

    add the skill page

--- js/app.js
+++ js/app.js
@@ -51,4 +51,37 @@ setLogo();
 
 
 
+var oTop = document.getElementById('top'),
+            owhet = window.innerHeight,
+            obody = document.body;
 
+        // 当网页被卷过超过整个屏幕的高度时，切换top按钮的透明为1，不然就是0
+        function toggleOpacity() {
+            var stp = obody.scrollTop;
+            console.log(stp);
+            if (stp > owhet) {
+                oTop.style.display = "block";
+                setTimeout(function() {
+                    oTop.style.opacity = 1;
+                }, 10);
+            } else {
+                oTop.style.display = "none";
+                oTop.style.opacity = 0;
+            }
+        }
+
+        // 当点击top按钮时，每30毫秒减少当前stp的一半，直至为0时，清除计时器
+        function toTheTop() {
+            var timer = setInterval(function() {
+                var stp = obody.scrollTop;
+                if (stp > 0) {
+                    stp -= stp / 2;
+                    obody.scrollTop = stp;
+                } else {
+                    clearInterval(timer)
+                }
+            }, 30)
+        }
+
+        oTop.onclick = toTheTop;
+        obody.ontouchmove = obody.onscroll = toggleOpacity;
