729f313a1ab4788f424387d5e50cbbb40e7b9cb6
Cai heng <ianyaheng822@gmail.com>
2019-09-23 00:20:02 +1000

    add the about page animation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 // import simpleParallax from 'simple-parallax-js';
 // import simpleParallax from 'simple-parallax-js';
 
-const hambuger=document.querySelector(".menu_hambuger");
+// const hambuger=document.querySelector(".menu_hambuger");
 const navLinks=document.querySelector(".nav_links");
 const links=document.querySelectorAll(".nav_links li");
 const menu=document.querySelector(".menu");
@@ -10,19 +10,18 @@ const navInfo=document.querySelector(".info");
 const iconScroll=document.querySelector(".icon-scroll");
 
 
-hambuger.addEventListener("click",()=>{
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
-});
+// hambuger.addEventListener("click",()=>{
+//   navLinks.classList.toggle("open");
+//   links.forEach(link => {
+//     link.classList.toggle("fade");
+//   });
+// });
 
 
 menu.addEventListener("click",()=>{
   navLinks.classList.toggle("open");
   navInfo.classList.toggle("hidden")
   iconScroll.classList.toggle("hidden");
-  
 
 })
 
